5e846e9a8e2c68867650b80c19fad7cce2b52855
Buzinov Vsevolod <buzinvs@czc72042s7.(none)>
2009-11-26 10:39:36 +0200

    Added linux-desktop configuration

---
+++ linux-desktop.sh
@@ -0,0 +1,20 @@
+# Usage:
+#   echo ". ~/.bash/remote.sh" > ~/.bash_login
+
+dir=`dirname $BASH_SOURCE`
+
+. $dir/common/terminal.sh
+. $dir/common/path.sh
+. $dir/common/misc-env.sh
+. $dir/common/misc-shortcuts.sh
+
+. $dir/server/range.sh
+. $dir/server/misc.sh
+
+. $dir/common/git.sh
+
+
+export PATH=$PATH:$dir/linux-desktop/
+. $dir/linux-desktop/osx-compat.sh
+
+
---
+++ linux-desktop/osx-compat.sh
@@ -0,0 +1,3 @@
+alias open="nautilus"
+alias mate="g"
+
